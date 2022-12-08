9f45450cb577cc1438396c0f3fd0fd962ad6daf8
Алексей <114584355+Dreddsol@users.noreply.github.com>
2022-12-09 00:04:37 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ console.log(arr.length);
 for (let i = 0; i < arr.length; i++){
     console.log(arr[i]);
 }*/
-let inputValue = prompt("Input value");
+/*let inputValue = prompt("Input value");
 console.log(inputValue);
 
 if ((typeof(+inputValue) == 'number') && (!isNaN(+inputValue))){
@@ -15,4 +15,24 @@ if ((typeof(+inputValue) == 'number') && (!isNaN(+inputValue))){
     }
 } else {
     console.log("oops it seems you were wrong");
+}*/
+/*const person={
+    city: 'Moscow'
 }
+const student =Object.create(person);
+student.ownCity = 'Piter';
+for(let key in student){
+    if(student.hasOwnProperty(key)){
+    console.log(key)
+}
+}
+console.log(key)*/
+function repeatWord(word, count) {
+	let words = '';
+  for (let i = 1; i <= count; --i) {
+  	words += word + i + ', ';
+  }
+  alert(words);
+}
+
+repeatWord('слово', 3)
