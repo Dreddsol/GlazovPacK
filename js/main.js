20e762d2dc225ce150046a3832ff1d140b5b01bb
Алексей <114584355+Dreddsol@users.noreply.github.com>
2022-12-15 23:49:08 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,9 @@
-/*let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-console.log(arr.length);
-
-for (let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
-}*/
-/*let inputValue = prompt("Input value");
-console.log(inputValue);
-
-if ((typeof(+inputValue) == 'number') && (!isNaN(+inputValue))){
-    if (inputValue % 2 === 0) {
-        console.log("Number is even");
-    } else {
-        console.log("Number is odd");
-    }
-} else {
-    console.log("oops it seems you were wrong");
-}*/
-/*const person={
-    city: 'Moscow'
-}
-const student =Object.create(person);
-student.ownCity = 'Piter';
-for(let key in student){
-    if(student.hasOwnProperty(key)){
-    console.log(key)
-}
-}
-console.log(key)*/
-function repeatWord(word, count) {
-	let words = '';
-  for (let i = 1; i <= count; --i) {
-  	words += word + i + ', ';
-  }
-  alert(words);
-}
-
-repeatWord('слово', 3)
+new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dinamicBullets:true,
+    },
+      });
+      document.querySelector('swiper-pagination-bullet');
+      i
